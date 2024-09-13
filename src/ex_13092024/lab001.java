1181cf5a6175e09d993b3d1b5635136f5d7b14ed
OrCon <smattaprthi@digisprint.com>
2024-09-13 11:45:57 +0530

    Task 13092024

--- src/ex_13092024/lab001.java
+++ src/ex_13092024/lab001.java
@@ -1,3 +1,5 @@
+package ex_13092024;
+
 public class lab001 {
     public static void main(String[] args) {
         System.out.println("Hello");
--- src/ex_13092024/lab002.java
+++ src/ex_13092024/lab002.java
@@ -1,4 +1,4 @@
-/* Write a program to print the table of number which user will give( int num 10)
+package ex_13092024;/* Write a program to print the table of number which user will give( int num 10)
 output will be like this by using the printf() ,
 10 × 1 = 10
 10 × 2 = 20
