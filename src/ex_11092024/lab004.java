cb5dadbbc3f14255c6ad3cd49ec3b1b6fd0f9803
OrCon <smattaprthi@digisprint.com>
2024-09-13 14:32:07 +0530

    Datatypes

---
+++ src/ex_11092024/lab004.java
@@ -0,0 +1,15 @@
+/** Boolean values
+ *
+ */
+package ex_11092024;
+
+import java.util.Arrays;
+
+public class lab004 {
+    public static void main(String[] args) {
+        boolean is_empty=true;
+        boolean is_empty_bottle=false;
+        System.out.println("Bottle is Empty = " + is_empty);
+        System.out.println("Bottle is Empty = " + is_empty_bottle);
+    }
+}
---
+++ src/ex_11092024/lab005.java
@@ -0,0 +1,30 @@
+package ex_11092024;
+
+import java.util.Arrays;
+
+public class lab005 {
+    public static void main(String[] args) {
+        //examples datatype Variable_name=variable_value
+        boolean glass_is_empty= true; //Valid Variable name
+        boolean Glass_is_empty=false; //Valid Variable name
+        byte b=100;
+        byte s=127;
+        char c='A';
+       // int 123=20; invalid Variable name 123
+      //  int 12num=10; invalid variable name 12num
+        //char 12_num='A'; invalid variable name 12num
+        int $=10; // vaild variable_name
+        int _x=5; // vaild variable_name
+        int NUM=200;
+        double d=10d;
+        System.out.println("Print the Boolean value :" +glass_is_empty);
+        System.out.println("Print the Boolean value :" +Glass_is_empty);
+        System.out.println("Print the integer value :" +$);
+        System.out.println("Print the integer value _x:" +_x);
+        System.out.println("Print the double value :" +d);
+        System.out.println("Print the Character value :" +c);
+
+
+
+    }
+}
