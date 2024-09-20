038190c3d181a0c4ac67d22b05b096a45e2e690a
OrCon <smattaprthi@digisprint.com>
2024-09-20 18:17:56 +0530

    Ternary Operator Multiple Conditions

---
+++ src/ex_20092024/lab_021.java
@@ -0,0 +1,14 @@
+package ex_20092024;
+public class lab_021 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int max=(a>b)?a:b;
+        System.out.println("Maximum Value of a & b is:" + max);
+        int min=(a<b)?a:b;
+        System.out.println("Minimum Value of a & b is:" + min);
+
+    }
+
+
+}
